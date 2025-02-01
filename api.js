fa6b76d4df720a98bb6a9d09e622b482eda1c7f2
nerkean <147550511+nerkean@users.noreply.github.com>
2025-02-01 17:05:58 +0200

    Update api.js

--- api.js
+++ api.js
@@ -14,6 +14,15 @@ const { Client, IntentsBitField } = require('discord.js');
 const NodeCache = require('node-cache');
 const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: "Слишком много запросов с вашего IP, пожалуйста, попробуйте позже.",
+  handler: (req, res, next, options) => {
+    res.status(429).setHeader('Retry-After', Math.ceil(options.windowMs / 1000)).send(options.message);
+  }
+});
+
 const cache = new NodeCache({ stdTTL: 60 * 60 });
 const client = new Client({
     intents: [
@@ -37,6 +46,8 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
+
+app.use(limiter);
 
 app.use(express.json());
 
