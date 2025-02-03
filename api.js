88079c1140e5e6067032e1cbaa77e847f0d2d48c
nerkean <147550511+nerkean@users.noreply.github.com>
2025-02-03 12:51:30 +0200

    Update api.js

--- api.js
+++ api.js
@@ -127,7 +127,7 @@ async function fetchUserGuildMember(userId) {
 passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'https://zeynbot3.onrender.com/auth/callback',
+    callbackURL: 'https://api.bandazeyna.com/auth/callback',
     scope: ['identify', 'guilds.members.read']
 },
 async (accessToken, refreshToken, profile, done) => {
